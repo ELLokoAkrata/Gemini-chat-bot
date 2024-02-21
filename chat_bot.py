322c63ff3ae9cfd6598b9a43f40b903923efcec4
RDvibe <91291838+RDvibe@users.noreply.github.com>
2024-02-21 14:16:43 -0500

    Create chat_bot.py

---
+++ chat_bot.py
@@ -0,0 +1,229 @@
+import os
+import time
+import streamlit as st
+import firebase_admin
+import uuid
+from firebase_admin import credentials, firestore
+from datetime import datetime
+import google.generativeai as genai  # Importación para Google Generative AI
+
+# Acceder a las credenciales de Firebase almacenadas como secreto
+firebase_secrets = st.secrets["firebase"]
+
+# Crear un objeto de credenciales de Firebase con los secretos
+cred = credentials.Certificate({
+    "type": firebase_secrets["type"],
+    "project_id": firebase_secrets["project_id"],
+    "private_key_id": firebase_secrets["private_key_id"],
+    "private_key": firebase_secrets["private_key"].replace('\\n', '\n'),
+    "client_email": firebase_secrets["client_email"],
+    "client_id": firebase_secrets["client_id"],
+    "auth_uri": firebase_secrets["auth_uri"],
+    "token_uri": firebase_secrets["token_uri"],
+    "auth_provider_x509_cert_url": firebase_secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": firebase_secrets["client_x509_cert_url"]
+})
+
+# Inicializar la aplicación de Firebase con las credenciales
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+# Acceder a la base de datos de Firestore
+db = firestore.client()
+
+# Configurar Google Generative AI con la clave API obtenida de los secretos de Streamlit
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Configuración del modelo de Google Generative AI
+model_name = 'gemini-1.0-pro'
+harassment_setting = 'block_none'
+temperature = 0.8
+top_p = 1
+top_k = 1
+max_output_tokens = 1024
+
+model = genai.GenerativeModel(
+    model_name=model_name,
+    safety_settings={'HARASSMENT': harassment_setting},
+    generation_config={
+        "temperature": temperature,
+        "top_p": top_p,
+        "top_k": top_k,
+        "max_output_tokens": max_output_tokens
+    }
+)
+
+# Iniciar una sesión de chat con el modelo de Google Generative AI
+chat = model.start_chat(history=[])
+
+# Inicialización de la sesión de Streamlit si es necesario
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+
+# Display logo
+logo_url = "https://firebasestorage.googleapis.com/v0/b/diario-ad840.appspot.com/o/Logo%20Netsat%20(1).jpg?alt=media&token=1a991e1d-cbdb-47a3-a4d9-d89e5d96a303"
+st.image(logo_url, use_column_width=True)
+
+with st.sidebar:
+    st.write("Netsat AI bot es un proyecto de IA + telecomunicaciones")
+    st.write("Se encuentra en etapa beta.")
+    st.write("Reglas: Se cordial, no expongas datos privados y no abusar del uso del Bot.")
+    st.write("El bot usa tecnología IA de Google llamada Gemini.")
+    st.write("El Bot se puede equivocar, siempre contrasta la info.")
+
+# Generar o recuperar el UUID del usuario
+if "user_uuid" not in st.session_state:
+    st.session_state["user_uuid"] = str(uuid.uuid4())
+
+# Configuración inicial de Firestore
+now = datetime.now()
+collection_name = "netsat_gemini" + now.strftime("%Y-%m-%d")
+document_name = st.session_state.get("user_uuid", str(uuid.uuid4()))
+collection_ref = db.collection(collection_name)
+document_ref = collection_ref.document(document_name)
+
+st.title("Netsat AI bot con Gemini 🤖")
+
+# Primero, renderizar el contenido con markdown en rojo
+st.markdown(""" Este es un bot que usa Gemini AI la tecnología de Inteligencia Artificial de Google, traído a este espacio gracias a Netsat SRL
+
+Guía para usar el bot:
+
+1) Coloca el nombre que quieras usar para el registro y presiona confirmar. No te preocupes si en la primera sesión dice: 'None'.
+
+2) Luego de iniciar sesión, escribe tu mensaje en la casilla especial y presiona el botón enviar.
+
+3) Luego espera la respuesta, y después de que el bot responda, borra el mensaje y escribe tu nuevo mensaje.
+
+4) Cuando ya no quieras hablar con el bot, cierra sesión.
+
+5) Siempre usa el mismo nombre de sesión, esto te ayudará a recuperar la sesión.
+6)Disfruta y pregunta lo que quieras preguntar.""")
+
+# Inicializar st.session_state
+if "user_uuid" not in st.session_state:
+    st.session_state["user_uuid"] = None  # Cambiado a None inicialmente
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+if "user_name" not in st.session_state:
+    st.session_state["user_name"] = None
+
+# Configuración inicial de Firestore
+now = datetime.now()
+collection_name = "netsat_" + now.strftime("%Y-%m-%d")
+document_name = st.session_state.get("user_uuid", str(uuid.uuid4()))
+collection_ref = db.collection(collection_name)
+document_ref = collection_ref.document(document_name)
+
+# Meta prompt para contextualizar al modelo sobre Netsat SRL y su especialidad en telecomunicaciones
+meta_prompt = "Este es un bot de Gemini para Netsat SRL, una empresa especializada en telecomunicaciones. Sin embargo, también puede ayudar en otros temas o consultas."
+
+# Internal prompt que combina el meta prompt con el historial de mensajes del usuario
+def build_internal_prompt(user_message, messages):
+    # Concatenar el meta prompt con el historial de mensajes del usuario
+    prompt = meta_prompt + "\n\n"
+    for msg in messages:
+        prompt += f"Usuario: {msg['content']}\n"
+    prompt += f"Usuario: {user_message}\n\n"
+    return prompt
+
+# Gestión del Inicio de Sesión
+if not st.session_state.get("logged_in", False):
+    user_name = st.text_input("Introduce tu nombre para comenzar")
+    confirm_button = st.button("Confirmar")
+    if confirm_button and user_name:
+        # Buscar en Firestore si el nombre de usuario ya existe
+        user_query = db.collection("usuarios").where("nombre", "==", user_name).get()
+        if user_query:
+            # Usuario existente encontrado, usar el UUID existente
+            user_info = user_query[0].to_dict()
+            st.session_state["user_uuid"] = user_info["user_uuid"]
+            st.session_state["user_name"] = user_name
+        else:
+            # Usuario nuevo, generar un nuevo UUID
+            new_uuid = str(uuid.uuid4())
+            st.session_state["user_uuid"] = new_uuid
+            user_doc_ref = db.collection("usuarios").document(new_uuid)
+            user_doc_ref.set({"nombre": user_name, "user_uuid": new_uuid})
+        st.session_state["logged_in"] = True
+
+        # Forzar a Streamlit a reejecutar el script aquí también después de crear un nuevo usuario
+        st.rerun()
+
+# Solo mostrar la interfaz si el usuario está "logged_in"
+if st.session_state.get("logged_in", False):
+    st.write(f"Bienvenido de nuevo, {st.session_state.get('user_name', 'Usuario')}!")
+    
+    doc_data = document_ref.get().to_dict()
+    if doc_data and 'messages' in doc_data:
+        st.session_state['messages'] = doc_data['messages']
+    
+    with st.container(border=True):
+        st.markdown("### Historial de Conversación")
+        for msg in st.session_state['messages']:
+            col1, col2 = st.columns([1, 5])  # Ajusta las proporciones según necesites
+            if msg["role"] == "user":
+                with col1:
+                    st.markdown("**Tú 🧑:**")
+                with col2:
+                    st.info(msg['content'])  # Usa st.info para los mensajes del usuario
+            else:
+                with col1:
+                    st.markdown("**IA 🤖:**")
+                with col2:
+                    st.success(msg['content'])  # Usa st.success para los mensajes de la IA
+
+    # Inicialización del historial de mensajes si es necesario
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = []
+
+    # Entrada de mensaje del usuario con st.chat_input
+    user_message = st.chat_input("Escribe tu mensaje aquí:", key="user_message")
+
+    # Verificar si el usuario ha enviado un mensaje
+if user_message:
+    # Agregar mensaje del usuario al historial
+    st.session_state['messages'].append({"role": "user", "content": user_message})
+
+    # Iniciar el spinner
+    with st.spinner('El bot está pensando...'):
+
+        # Construir el internal prompt con el meta prompt y el historial de mensajes del usuario
+        internal_prompt = build_internal_prompt(user_message, st.session_state['messages'])
+
+        # Llamada a la función de generación de contenido de la IA (ajusta según tu implementación)
+        response = model.generate_content(internal_prompt)  # Asegúrate de ajustar esta línea según tu modelo específico
+
+        # Procesar y mostrar la respuesta de la IA
+        if response and hasattr(response, 'candidates') and len(response.candidates) > 0:
+            # Suponiendo que los candidatos contienen objetos 'Part' con texto
+            ia_message_parts = response.candidates[0].content.parts
+            ia_message = " ".join(part.text for part in ia_message_parts if hasattr(part, 'text'))
+        else:
+            ia_message = "La respuesta no tiene el formato esperado o está vacía."
+
+        # Agregar respuesta de la IA al historial
+        st.session_state['messages'].append({"role": "assistant", "content": ia_message})
+        # Actualizar el almacenamiento o base de datos según sea necesario
+        document_ref.set({'messages': st.session_state['messages']})
+
+        # Mostrar la respuesta de la IA inmediatamente
+        st.write("IA:", ia_message)
+
+    # Gestión del cierre de sesión
+    if st.button("Cerrar Sesión"):
+        keys_to_keep = []  # Lista de claves del estado de sesión a mantener
+
+        # Borrar todas las claves del estado de sesión excepto las especificadas
+        for key in list(st.session_state.keys()):
+            if key not in keys_to_keep:
+                del st.session_state[key]
+
+        # Mensaje de sesión cerrada y re-ejecución del script para actualizar la interfaz de usuario
+        st.write("Sesión cerrada. ¡Gracias por usar el bot!")
+        st.rerun()
+
